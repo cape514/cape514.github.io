e2d9d0f09af40b207801f7c49f9712f272ecb20f
lpowell70 <62767395+lpowell70@users.noreply.github.com>
2020-04-05 17:16:21 -0400

    Created php file
    This file will allow an email to be sent to the union's desired email. It is a lttle work around from a more complex version. The only down sides are that if the form gets more complex this might not be the best way forward. Also emails at firs may be sent to the spam folder.

---
+++ mail.php
@@ -0,0 +1,17 @@
+<?php
+if(isset( $_POST['name']))
+$name = $_POST['name'];
+if(isset( $_POST['email']))
+$email = $_POST['email'];
+if(isset( $_POST['subject']))
+$subject = $_POST['subject'];
+if(isset( $_POST['message']))
+$message = $_POST['message'];
+
+
+$content="From: $name, $message ";
+$recipient = "union@example.com";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $content, $mailheader) or die("Error!");
+echo "Confirmation: Email sent! Thank you we will be in touch as soon as possible.";
+?>
